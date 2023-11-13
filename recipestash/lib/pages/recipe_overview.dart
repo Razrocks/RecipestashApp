5d36a129ddca931ff798eefdc1fee1da65c92e2a
AymanZahid <Ayman.Zahid@ontariotechu.net>
2023-11-12 19:22:13 -0500

    Modified recipe_overview UI

--- recipestash/lib/pages/recipe_overview.dart
+++ recipestash/lib/pages/recipe_overview.dart
@@ -15,24 +15,52 @@ class _RecipeOverviewState extends State<RecipeOverview> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.recipe.title ?? 'Recipe Overview'),
+        backgroundColor: Colors.white,
+        leading: IconButton(
+          icon: const Icon(
+            Icons.arrow_back,
+            color: Colors.black,
+          ),
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        ),
+        title: const Text(
+          'Recipe Overview',
+          style: TextStyle(
+            color: Colors.black,
+          ),
+        ),
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            // Display recipe details here
-            Text('Title: ${widget.recipe.title}'),
-            Text('Category: ${widget.recipe.category}'),
+            // Overview Section
+            _buildSectionTitle('Overview'),
+            _buildRecipeDetail('Title', widget.recipe.title),
+            _buildRecipeDetail('Category', widget.recipe.category),
             Text('Description: ${widget.recipe.description}'),
             Text('Prep Time: ${widget.recipe.prepTime}'),
             Text('Cook Time: ${widget.recipe.cookTime}'),
             Text('Servings: ${widget.recipe.servings}'),
+
+            // Ingredients Section
+            _buildSectionTitle('Ingredients'),
             Text('Ingredients: ${widget.recipe.ingredents}'),
+
+            // Directions Section
+            _buildSectionTitle('Directions'),
             Text('Directions: ${widget.recipe.directions}'),
+
+            // Miscellaneous Section
+            _buildSectionTitle('Miscellaneous'),
             Text('Notes: ${widget.recipe.notes}'),
             Text('Image URL: ${widget.recipe.imageUrl}'),
+
+            // Nutrition Section
+            _buildSectionTitle('Nutrition'),
             Text('Serving Size: ${widget.recipe.servingSize}'),
             Text('Calories: ${widget.recipe.calories}'),
             Text('Total Fat: ${widget.recipe.totalfat}'),
@@ -46,6 +74,46 @@ class _RecipeOverviewState extends State<RecipeOverview> {
             Text('Protein: ${widget.recipe.protein}'),
           ],
         ),
+      ),
+    );
+  }
+
+  Widget _buildSectionTitle(String title) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 12.0),
+      child: Text(
+        title,
+        style: const TextStyle(
+          color: Colors.black,
+          fontSize: 24,
+          fontWeight: FontWeight.bold,
+        ),
+      ),
+    );
+  }
+
+  Widget _buildRecipeDetail(String label, String? value) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 8.0),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Text(
+            label,
+            style: const TextStyle(
+              color: Colors.black,
+              fontSize: 18,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          Text(
+            value ?? '-',
+            style: const TextStyle(
+              color: Colors.black,
+              fontSize: 18,
+            ),
+          ),
+        ],
       ),
     );
   }
