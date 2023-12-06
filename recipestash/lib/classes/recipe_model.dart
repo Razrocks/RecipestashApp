2100f4cd98d986892501406617d1150d9fc90a15
AymanZahid <Ayman.Zahid@ontariotechu.net>
2023-12-05 20:21:35 -0500

    Filter recipes by category, and display the cards correctly

--- recipestash/lib/classes/recipe_model.dart
+++ recipestash/lib/classes/recipe_model.dart
@@ -34,56 +34,56 @@ class RecipeModel {
     dietaryFiber,
     sugar,
     protein,
-    ) async {
-      Recipe data = Recipe(
-        title: title,
-        category: category,
-        description: description,
-        prepTime: prepTime,
-        cookTime: cookTime,
-        servings: servings,
-        ingredents: ingredients,
-        directions: directions,
-        notes: notes,
-        imageUrl: imageUrl,
-        servingSize: servingSize,
-        calories: calories,
-        totalfat: totalfat,
-        saturatedFat: saturatedFat,
-        transFat: transFat,
-        cholesterol: cholesterol,
-        sodium: sodium,
-        totalCarbohydrates: totalCarbohydrates,
-        dietaryFiber: dietaryFiber,
-        sugar: sugar,
-        protein: protein,
-      );
-      DocumentReference? ref = await db?.add(data.toMap());
-      updateRecipe(
-        ref?.id,
-        title,
-        category,
-        description,
-        prepTime,
-        cookTime,
-        servings,
-        ingredients,
-        directions,
-        notes,
-        imageUrl,
-        servingSize,
-        calories,
-        totalfat,
-        saturatedFat,
-        transFat,
-        cholesterol,
-        sodium,
-        totalCarbohydrates,
-        dietaryFiber,
-        sugar,
-        protein,
-      );
-    }
+  ) async {
+    Recipe data = Recipe(
+      title: title,
+      category: category,
+      description: description,
+      prepTime: prepTime,
+      cookTime: cookTime,
+      servings: servings,
+      ingredents: ingredients,
+      directions: directions,
+      notes: notes,
+      imageUrl: imageUrl,
+      servingSize: servingSize,
+      calories: calories,
+      totalfat: totalfat,
+      saturatedFat: saturatedFat,
+      transFat: transFat,
+      cholesterol: cholesterol,
+      sodium: sodium,
+      totalCarbohydrates: totalCarbohydrates,
+      dietaryFiber: dietaryFiber,
+      sugar: sugar,
+      protein: protein,
+    );
+    DocumentReference? ref = await db?.add(data.toMap());
+    updateRecipe(
+      ref?.id,
+      title,
+      category,
+      description,
+      prepTime,
+      cookTime,
+      servings,
+      ingredients,
+      directions,
+      notes,
+      imageUrl,
+      servingSize,
+      calories,
+      totalfat,
+      saturatedFat,
+      transFat,
+      cholesterol,
+      sodium,
+      totalCarbohydrates,
+      dietaryFiber,
+      sugar,
+      protein,
+    );
+  }
 
   Future<void> updateRecipe(
     id,
@@ -108,34 +108,34 @@ class RecipeModel {
     dietaryFiber,
     sugar,
     protein,
-    ) async {
-      DocumentReference ref = db!.doc(id);
-      Recipe data = Recipe(
-        id: id,
-        title: title,
-        category: category,
-        description: description,
-        prepTime: prepTime,
-        cookTime: cookTime,
-        servings: servings,
-        ingredents: ingredients,
-        directions: directions,
-        notes: notes,
-        imageUrl: imageUrl,
-        servingSize: servingSize,
-        calories: calories,
-        totalfat: totalfat,
-        saturatedFat: saturatedFat,
-        transFat: transFat,
-        cholesterol: cholesterol,
-        sodium: sodium,
-        totalCarbohydrates: totalCarbohydrates,
-        dietaryFiber: dietaryFiber,
-        sugar: sugar,
-        protein: protein,
-      );
-      await ref.update(data.toMap());
-    }
+  ) async {
+    DocumentReference ref = db!.doc(id);
+    Recipe data = Recipe(
+      id: id,
+      title: title,
+      category: category,
+      description: description,
+      prepTime: prepTime,
+      cookTime: cookTime,
+      servings: servings,
+      ingredents: ingredients,
+      directions: directions,
+      notes: notes,
+      imageUrl: imageUrl,
+      servingSize: servingSize,
+      calories: calories,
+      totalfat: totalfat,
+      saturatedFat: saturatedFat,
+      transFat: transFat,
+      cholesterol: cholesterol,
+      sodium: sodium,
+      totalCarbohydrates: totalCarbohydrates,
+      dietaryFiber: dietaryFiber,
+      sugar: sugar,
+      protein: protein,
+    );
+    await ref.update(data.toMap());
+  }
 
   Future<void> deleteRecipe(id) async {
     db?.doc(id).delete();
@@ -143,18 +143,31 @@ class RecipeModel {
 
   Stream<List<Recipe>> getAllRecipes() {
     return db!.snapshots().map((snapshot) => snapshot.docs.map((doc) {
-      return Recipe.fromMap(doc.data() as Map<String, dynamic>, reference: doc.reference);
-    }).toList());
+          return Recipe.fromMap(doc.data() as Map<String, dynamic>,
+              reference: doc.reference);
+        }).toList());
   }
 
   Future<Recipe> getRecipebyId(String id) async {
     DocumentSnapshot documentSnapshot = await db!.doc(id).get();
-    return Recipe.fromMap(documentSnapshot.data() as Map<String, dynamic>, reference: documentSnapshot.reference);
+    return Recipe.fromMap(documentSnapshot.data() as Map<String, dynamic>,
+        reference: documentSnapshot.reference);
   }
 
   Future<void> deleteAllRecipe() async {
     QuerySnapshot querySnapshot = await db!.get();
-    for (QueryDocumentSnapshot element in querySnapshot.docs) { 
-      element.reference.delete();}
+    for (QueryDocumentSnapshot element in querySnapshot.docs) {
+      element.reference.delete();
+    }
+  }
+
+  Stream<List<Recipe>> getRecipesByCategory(String category) {
+    return db!
+        .where('category', isEqualTo: category)
+        .snapshots()
+        .map((snapshot) => snapshot.docs.map((doc) {
+              return Recipe.fromMap(doc.data() as Map<String, dynamic>,
+                  reference: doc.reference);
+            }).toList());
   }
 }
