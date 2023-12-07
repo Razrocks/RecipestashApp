ebf761e8f8e145deb7a9e0f5f9a38f0f4d914b30
AymanZahid <Ayman.Zahid@ontariotechu.net>
2023-12-07 03:58:11 -0500

    Added comments for all files in the classes folder

--- recipestash/lib/classes/preferences.dart
+++ recipestash/lib/classes/preferences.dart
@@ -1,9 +1,14 @@
+// Preferences class representing user preferences for the Recipe app
 class Preferences {
+  // Attributes for storing RGB color values, dark mode preference, and notification preference
   int? r, g, b, darkMode, notifications;
 
+  // Constructor for creating Preferences objects with optional parameters
   Preferences({this.r, this.g, this.b, this.darkMode, this.notifications});
 
+  // Constructor to create Preferences object from a map of key-value pairs
   Preferences.fromMap(Map<String, dynamic> map) {
+    // Assign values from the map to corresponding attributes
     r = map['r'];
     g = map['g'];
     b = map['b'];
@@ -11,6 +16,7 @@ class Preferences {
     notifications = map['notifications'];
   }
 
+  // Method to convert Preferences object to a map of key-value pairs
   Map<String, dynamic> toMap() {
     return {
       'r': r,
@@ -21,6 +27,7 @@ class Preferences {
     };
   }
 
+  // Override toString method to provide a string representation of Preferences object
   @override
   String toString() {
     return 'Preferences{r: $r, g: $g, b: $b, darkMode: $darkMode, notifications: $notifications}';
--- recipestash/lib/classes/preferences_model.dart
+++ recipestash/lib/classes/preferences_model.dart
@@ -1,12 +1,19 @@
+// Import necessary packages and utilities for SQLite and preferences
 import 'package:sqflite/sqflite.dart';
 import 'package:recipestash/utilities/sqflite_utils.dart';
 import 'package:recipestash/classes/preferences.dart';
 
+// PreferencesModel class responsible for interacting with user preferences in SQFLite database
 class PreferencesModel {
+  // Method to check if preferences entry exists in the database
   Future<int> exists() async {
+    // Initialize the SQFLite database using SqfliteUtils
     final Database db = await SqfliteUtils.init();
+
+    // Query the 'preferences' table and check if the result is empty
     List<Map<String, dynamic>> result = await db.query('preferences');
 
+    // Return 0 if no preferences entry found, otherwise return 1
     if (result.isEmpty) {
       return 0;
     } else {
@@ -14,9 +21,12 @@ class PreferencesModel {
     }
   }
 
+  // Method to update existing preferences in the database
   Future<void> update(Preferences preferences) async {
+    // Initialize the SQLite database using SqfliteUtils
     final Database db = await SqfliteUtils.init();
 
+    // Update the 'preferences' table with the new preferences data
     await db.update(
       'preferences',
       preferences.toMap(),
@@ -26,9 +36,12 @@ class PreferencesModel {
     );
   }
 
-  Future<void> create(Preferences  preferences) async {
+  // Method to create a new preferences entry in the database
+  Future<void> create(Preferences preferences) async {
+    // Initialize the SQFLite database using SqfliteUtils
     final Database db = await SqfliteUtils.init();
 
+    // Insert the new preferences data into the 'preferences' table
     await db.insert(
       'preferences',
       preferences.toMap(),
@@ -36,9 +49,12 @@ class PreferencesModel {
     );
   }
 
+  // Method to retrieve preferences from the database
   Future<Preferences> get() async {
+    // Initialize the SQFLite database using SqfliteUtils
     final Database db = await SqfliteUtils.init();
 
+    // Query the 'preferences' table and convert the result to a Preferences object
     List<Map<String, dynamic>> result = await db.query('preferences');
     return Preferences.fromMap(result[0]);
   }
